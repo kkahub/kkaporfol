cad09fa18f31fc640c4d5870dfd9ab8df4153ea3
kkahub <tndosdtn1!@>
2023-09-01 21:35:56 +0900

    초기세팅 및 세팅 테스트

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const path = require("path");
 
-module.exports = nextConfig
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  sassOptions: {
+    includePaths: [path.join(__dirname, "src/styles")],
+    prependData:
+      "@import 'src/styles/_variables.scss'; @import 'src/styles/_mixins.scss'; @import 'src/styles/_fonts.scss'; @import 'src/styles/_reset.scss';",
+  },
+};
+
+module.exports = nextConfig;
---
+++ src/components/layout/Header.tsx
@@ -0,0 +1,39 @@
+import Link from "next/link";
+
+export default function Header() {
+  return (
+    <>
+      <header id="header" className="">
+        <div className="inner">
+          <h1>
+            <Link href="/">KKA</Link>
+          </h1>
+          <nav id="nav">
+            <div className="burgur">
+              <span className="bar1"></span>
+              <span className="bar2"></span>
+              <span className="bar3"></span>
+            </div>
+            <ul className="gnb">
+              <li className="on">
+                <Link href="/">INTRO</Link>
+              </li>
+              <li>
+                <Link href="/profile">PROFILE</Link>
+              </li>
+              <li>
+                <Link href="/skill">SKILL</Link>
+              </li>
+              <li>
+                <Link href="/portfolio">PORTFOLIO</Link>
+              </li>
+              <li>
+                <Link href="/preview">PREVIEW</Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </header>
+    </>
+  );
+}
