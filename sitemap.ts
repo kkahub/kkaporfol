8a974e999e7a23de639b7c8ffdef5111cc311cea
kkahub <tndosdtn1!@>
2023-11-09 04:36:10 +0900

    잘잘한 수정사항 적용

--- sitemap.ts
+++ sitemap.ts
@@ -3,7 +3,7 @@ import { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://kkahub.github.io/kkaporfol/",
+      url: "https://kkahub.github.io/kkaporfol",
       lastModified: new Date(),
       changeFrequency: "yearly",
       priority: 1,
