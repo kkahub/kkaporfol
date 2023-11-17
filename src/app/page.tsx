107cc25309978fe3f5e441f27c772a773f09d074
kkahub <tndosdtn1!@>
2023-11-18 00:42:14 +0900

    Next.js v14로 업그레이드 및 변경사항 반영

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import { Metadata } from "next";
 import Intro from "./intro/page";
 
 export const metadata: Metadata = {
+  metadataBase: new URL("https://kkahub.github.io/kkaporfol"),
   title: "프론트엔드 개발자 김근애 포트폴리오",
   description: "김근애의 프론트엔드 개발, 웹퍼블리싱 포트폴리오 사이트입니다.",
   authors: [{ name: "김근애", url: "https://kkahub.github.io/kkaporfol" }],
@@ -17,11 +18,9 @@ export const metadata: Metadata = {
     title: "KKA Front-end Developer Portfolio",
     description:
       "김근애의 프론트엔드 개발, 웹퍼블리싱 포트폴리오 사이트입니다.",
-    url: "https://kkahub.github.io/kkaporfol",
+    url: "/",
     siteName: "프론트엔드 개발자 김근애 포트폴리오",
-    images: [
-      { url: "https://kkahub.github.io/kkaporfol/images/visual_bg1_m.jpg" },
-    ],
+    images: [{ url: "/images/visual_bg1_m.jpg" }],
     type: "website",
     locale: "ko_KR",
   },
